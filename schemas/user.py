bf211c82eb99d95ad501952f4de071f51b0977e7
mrx <meibrohimov@gmail.com>
2025-04-11 11:12:22 +0500

    add post method

--- schemas/user.py
+++ schemas/user.py
@@ -5,3 +5,16 @@ class BaseUser(BaseModel):
     name: str
     email: EmailStr
     username: str
+
+    class Config:
+        orm_mode = True
+
+
+class UserOut(BaseModel):
+    id: int
+    name: str
+    email: str
+    username: str
+
+    class Config:
+        from_attributes = True
